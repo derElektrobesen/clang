ad41449e6c5c9542842b2f0d1a21ca17108a01e8
Pavel Berezhnoy <pberejnoy2005@gmail.com>
2016-04-17 23:00:07 +0300

    All directives parsing implemented

--- lib/Parse/ParseTnt.cpp
+++ lib/Parse/ParseTnt.cpp
@@ -44,6 +44,7 @@ namespace {
 
     TokenReplacement(std::string const& FuncName, uint64_t ArrLen)
       : FuncName(FuncName), ArrLen(ArrLen) {}
+    TokenReplacement() : ArrLen(0) {}
   };
 } // namespace
 
@@ -52,7 +53,6 @@ static TntDirectiveKind getTntDirectiveKind(Parser &P) {
   if (Tok.isAnnotation())
     return TntDK_unknown;
 
-  llvm::errs() << P.getPreprocessor().getSpelling(Tok);
   return llvm::StringSwitch<TntDirectiveKind>(P.getPreprocessor().getSpelling(Tok))
 #define XXX(d) .Case(#d, TntDK_ ## d)
         TNT_DIRECTIVE_KINDS(XXX)
@@ -77,18 +77,18 @@ static const char *tntDirectiveToStr(TntDirectiveKind d) {
   return nullptr;
 }
 
-static std::string mkFuncName(std::string const& name) {
-  return "__tnt_" + name + "_type_converter";
+static std::string mkFuncName(std::string const& name, bool is_formal) {
+  return "__tnt_" + name + (is_formal ? "_formal" : "") + "_type_converter";
 }
 
-static std::string typeToFuncName(QualType const& type, uint64_t *arr_size = nullptr, bool under_recursion = false) {
+static std::string typeToFuncName(QualType const& type, bool is_formal, uint64_t *arr_size = nullptr, bool under_recursion = false) {
   if (type->isScalarType()) {
     if (type->isCharType())
-      return mkFuncName("char");
+      return mkFuncName("char", is_formal);
     if (type->isIntegerType())
-      return mkFuncName("int");
+      return mkFuncName("int", is_formal);
     if (type->isRealFloatingType())
-      return mkFuncName("double");
+      return mkFuncName("double", is_formal);
 
     if (type->isPointerType()) {
       const PointerType *ptr_t = dyn_cast<PointerType>(type);
@@ -97,7 +97,7 @@ static std::string typeToFuncName(QualType const& type, uint64_t *arr_size = nul
 
       QualType pointee = ptr_t->getPointeeType();
       if (pointee->isCharType())
-        return mkFuncName("char_ptr"); // only c-strings are supported
+        return mkFuncName("char_ptr", is_formal); // only c-strings are supported
 
       return std::string();
     }
@@ -112,7 +112,7 @@ static std::string typeToFuncName(QualType const& type, uint64_t *arr_size = nul
       return std::string();
     if (arr_size)
       *arr_size = arr_t->getSize().getZExtValue();
-    return typeToFuncName(arr_t->getElementType(), nullptr, true);
+    return typeToFuncName(arr_t->getElementType(), is_formal, nullptr, true);
   }
 
   return std::string();
@@ -129,57 +129,57 @@ public:
     : P(P)
   {}
 
-  ///
-  ///     tnt-eval-directive
-  ///       annot_pragma_tnt
-  ///         eval ( <variable> ',' )* <function_call>
-  ///       annot_pragnma_tnt_end
-  ///
-  bool parseTnt_eval_Directive(SmallVectorImpl<TokenReplacement> &replace_with) {
-    P.SkipUntil(tok::annot_pragma_tnt_end);
-    P.ConsumeToken();
-    return false;
+  bool isPossiblyFormal() {
+    Token Tok = P.getCurToken();
+    return Tok.is(tok::question); // first token is '?'
   }
 
-  ///
-  ///     tnt-in-directive
-  ///       annot_pragma_tnt
-  ///         in (( <expr> | ?<variable> ) ',')* ( <expr> | ?<variable> )
-  ///       annot_pragma_tnt_end
-  ///
-  bool parseTnt_in_Directive(SmallVectorImpl<TokenReplacement> &replace_with) {
-    P.SkipUntil(tok::annot_pragma_tnt_end);
-    P.ConsumeToken();
-    return false;
+  StmtResult processPragma() {
+    Parser::TentativeParsingAction TPA(P);
+    P.ConsumeToken(); // annot_pragma_tnt
+
+    TntDirectiveKind dir = getTntDirectiveKind(P);
+    if (dir == TntDK_unknown) {
+      P.Diag(P.getCurToken(), diag::err_tnt_unknown_directive);
+      P.SkipUntil(tok::annot_pragma_tnt_end);
+      P.ConsumeToken();
+      TPA.Commit();
+      return StmtError();
+    }
+
+    // don't consume pragma type identifier
+    SmallVector<TokenReplacement, 16> repl;
+    if (!parsePragmaTokens(repl, dir)) {
+      TPA.Commit();
+      return StmtError();
+    }
+
+    TPA.Commit();
+
+    return StmtEmpty();
   }
 
-  ///
-  ///     tnt-rd-directive
-  ///       annot_pragma_tnt
-  ///         rd (( <expr> | ?<variable> ) ',')* ( <expr> | ?<variable> )
-  ///       annot_pragma_tnt_end
-  ///
-  bool parseTnt_rd_Directive(SmallVectorImpl<TokenReplacement> &replace_with) {
-    P.SkipUntil(tok::annot_pragma_tnt_end);
-    P.ConsumeToken();
-    return false;
-  }
-
-  ///
-  ///     tnt-out-directive
-  ///       annot_pragma_tnt
-  ///         out (( <expr> ) ',')* ( <expr> )
-  ///       annot_pragma_tnt_end
-  ///
-  bool parseTnt_out_Directive(SmallVectorImpl<TokenReplacement> &replace_with) {
+  bool parsePragmaTokens(SmallVectorImpl<TokenReplacement> &replace_with, TntDirectiveKind kind) {
     replace_with.clear();
+
+    assert(kind != TntDK_unknown);
+    bool accept_formals = (kind == TntDK_in || kind == TntDK_rd);
 
     Token Tok = P.getCurToken();
     SmallVector<TokenReplacement, 16> Replacement;
 
     Preprocessor &PP = P.getPreprocessor();
 
+    bool formal_found = false;
     bool correct = true;
+    bool is_first_iter = true;
+
+    auto _abort = [&correct] (Parser::TentativeParsingAction &TPA) {
+      TPA.Commit();
+      correct = false;
+    };
+
+
     while (Tok.isNot(tok::annot_pragma_tnt_end)) {
       Token ExprBoundary;
       ExprBoundary.startToken();
@@ -188,8 +188,17 @@ public:
       PP.EnterToken(ExprBoundary); // enter brace after comma or annot_pragma_tnt
                                    // to devide expressions
 
-      assert(Tok.isOneOf(tok::r_brace, tok::annot_pragma_tnt));
+      PP.DumpToken(P.getCurToken());
+      llvm::errs() << "\n";
+
+      if (is_first_iter)
+        assert(Tok.is(tok::identifier));
+      else
+        assert(Tok.is(tok::r_brace));
+
       P.ConsumeAnyToken();
+      is_first_iter = false;
+
       Tok = P.getCurToken();
       if (Tok.is(tok::annot_pragma_tnt_end))
         break;
@@ -197,28 +206,58 @@ public:
       Parser::TentativeParsingAction TPA(P);
       P.ConsumeBrace();
 
+      bool is_formal = isPossiblyFormal();
+      Token formalTok = P.getCurToken();
+      if (is_formal && !accept_formals) {
+        P.Diag(formalTok, diag::err_tnt_unsupported_formal);
+        _abort(TPA);
+        break;
+      } else if (is_formal) {
+        Tok = P.getCurToken();
+        assert(Tok.is(tok::question));
+        P.ConsumeToken();
+      }
+
       auto expr = P.ParseAssignmentExpression();
       if (expr.isInvalid()) {
         // error message already shawn
-        TPA.Commit();
-        correct = false;
+        _abort(TPA);
         break;
       }
 
       Tok = P.getCurToken();
+      if (is_formal) {
+        if (!isa<DeclRefExpr>(expr.get())) {
+          P.Diag(formalTok, diag::err_tnt_invalid_formal);
+          _abort(TPA);
+          break;
+        }
+
+        const DeclRefExpr *decl_ref = dyn_cast<DeclRefExpr>(expr.get());
+        if (!isa<VarDecl>(decl_ref->getDecl())) { // TODO: not only variables can be formal
+          P.Diag(formalTok, diag::err_tnt_invalid_formal);
+          _abort(TPA);
+          break;
+        }
+
+        if (decl_ref->getType().isConstQualified()) {
+          P.Diag(formalTok, diag::err_tnt_const_formal);
+          _abort(TPA);
+          break;
+        }
+      }
+
       uint64_t size = 0;
-      std::string func_name = typeToFuncName(expr.get()->getType(), &size);
+      std::string func_name = typeToFuncName(expr.get()->getType(), is_formal, &size);
       if (!func_name.size()) {
         P.Diag(Tok, diag::err_tnt_unsupported_type) << expr.get()->getType().getAsString();
-        TPA.Commit();
-        correct = false;
+        _abort(TPA);
         break;
       }
 
       if (Tok.isNot(tok::annot_pragma_tnt_end) && Tok.isNot(tok::comma)) {
         P.Diag(Tok, diag::err_expected_expression); // TODO: print correct message here
-        TPA.Commit();
-        correct = false;
+        _abort(TPA);
         break;
       }
 
@@ -233,10 +272,32 @@ public:
       P.ConsumeBrace(); // consume l_brace
 
       TokenReplacement repl(func_name, size);
+      Tok = P.getCurToken();
       assert(P.ConsumeAndStoreUntil(tok::r_brace, tok::annot_pragma_tnt_end, repl.Args, /*StopOnSemi*/true, /*ConsumeFinalToken*/false)
           && "Achtung! r_brace not found in stream!");
 
       repl.Args.pop_back(); // remove comma from args list
+
+      if (is_formal) {
+        Token l_paren;
+        l_paren.startToken();
+        l_paren.setKind(tok::l_paren);
+        l_paren.setLocation(Tok.getLocation());
+        repl.Args.insert(repl.Args.begin(), l_paren);
+
+        Token amp;
+        amp.startToken();
+        amp.setKind(tok::amp);
+        amp.setLocation(Tok.getLocation());
+        repl.Args.insert(repl.Args.begin(), amp);
+
+        Token r_paren;
+        r_paren.startToken();
+        r_paren.setKind(tok::r_paren);
+        r_paren.setLocation(repl.Args.back().getLocation());
+        repl.Args.push_back(r_paren);
+      }
+
       replace_with.push_back(repl);
 
       // XXX: Don't consume r_brace!!!
@@ -248,6 +309,7 @@ public:
     P.ConsumeToken(); // consume annot_pragma_tnt_end
 
     return correct;
+
   }
 };
 
@@ -262,29 +324,8 @@ public:
 ///
 StmtResult Parser::ParseTntDirective() {
   assert(Tok.is(tok::annot_pragma_tnt) && "Not a Tnt directive!");
-
-  ConsumeToken();
-
-  TntDirectiveKind dir = getTntDirectiveKind(*this);
-  if (dir == TntDK_unknown) {
-    Diag(Tok, diag::err_tnt_unknown_directive);
-    SkipUntil(tok::annot_pragma_tnt_end);
-    ConsumeToken();
-    return StmtError();
-  }
-
   TntParser parser(*this);
-  std::function<bool(SmallVectorImpl<TokenReplacement>&)> parsers[] = {
-#define XXX(d) \
-    [TntDK_ ## d] = [&parser] (SmallVectorImpl<TokenReplacement> &tokens) { return parser.parseTnt_ ## d ## _Directive(tokens); },
-    TNT_DIRECTIVE_KINDS(XXX)
-#undef XXX
-  };
-
-  SmallVector<TokenReplacement, 16> tokens;
-  bool res = parsers[dir](tokens);
-  if (!res)
-    return StmtError();
+  return parser.processPragma();
 }
 
 Parser::DeclGroupPtrTy Parser::HandleTntGlobalDirective() {
